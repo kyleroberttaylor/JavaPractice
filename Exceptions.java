e1d10f237f5f70c592889b665b77c88983e4850c
kyleroberttaylor <97183841+kyleroberttaylor@users.noreply.github.com>
2023-02-01 15:04:00 -0500

    Create Exceptions.java

---
+++ Exceptions.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Exceptions{
+  public static void main(String[] args){
+   int a,b,c;
+   Scanner scanner = new Scanner(System.in);
+   System.out.println("Please enter 3 integers:");
+   
+   
+   }
+}
